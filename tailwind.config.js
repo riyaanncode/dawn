ef8e54b207433ff8f95666b1151f35b67155a7be
riya <riya.anncode2@gmail.com>
2024-08-12 14:58:38 +0530

    Shopify Theme

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./assets/*.liquid",
+    "./layout/*.liquid",
+    "./section/*.liquid",
+    "./snippets/*.liquid",
+    "./templates/*.liquid",
+    "./templates/"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
